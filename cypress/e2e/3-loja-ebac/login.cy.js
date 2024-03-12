ff6336ce5dc0893202c901263e29df61f666023f
samu <kamusalss@gmail.com.>
2024-03-12 19:32:15 -0300

    faker e variáveis

--- cypress/e2e/3-loja-ebac/login.cy.js
+++ cypress/e2e/3-loja-ebac/login.cy.js
@@ -1,19 +1,17 @@
 ///<reference types="cypress"/>
-
 describe('Funcionalidade: Login', () => {
-
     beforeEach(() => {
         cy.visit ('http://lojaebac.ebaconline.art.br/minha-conta/')
     });
      afterEach(() => {
             cy.screenshot ()
     });
-    it('Deve fazer login com sucesso', () => {
+it('Deve fazer login com sucesso', () => {
         cy.get('#username').type('kamusalss@gmail.com')
         cy.get('#password').type('kamai666')
         cy.get('.woocommerce-form > .button').click()
         cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('contain','Olá, kamusalss')
-    })
+})
 it('Deve exibir uma mensagem de erro ao inserir usuário inválido', () => {
     cy.get('#username').type('kamusalsss@gmail.com')
     cy.get('#password').type('kamai666')
