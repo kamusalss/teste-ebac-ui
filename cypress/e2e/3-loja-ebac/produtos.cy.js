32009980ad4bdb8710ef9bb07abed72e7251822d
samu <kamusalss@gmail.com.>
2024-03-19 16:46:47 -0300

    teste de produtos adicionado

---
+++ cypress/e2e/3-loja-ebac/produtos.cy.js
@@ -0,0 +1,19 @@
+///<reference types="cypress"/>
+
+describe('Funcionalidade: Produtos', () => {
+
+    beforeEach(() => {
+        cy.visit ('http://lojaebac.ebaconline.art.br/produtos/')
+    });
+
+    it('Deve selecionar um produto da lista', () => {
+        cy.get('.product-block')
+            //.first()
+            //.last()
+            //.eq(2)
+            .contains('Abominable Hoodie')
+            .click()
+
+            cy.get('#tab-title-description > a').should('exist')
+    });
+});
